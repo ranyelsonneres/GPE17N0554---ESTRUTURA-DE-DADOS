9b90c3bb05ef4f4c96736420ddda5fa2938d8830
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2025-03-13 20:20:50 -0300

    Update insertion-sort.c

--- Aula_13_03_2025/insertion-sort.c
+++ Aula_13_03_2025/insertion-sort.c
@@ -16,11 +16,24 @@ void insertionSort(int arr[], int n){
 }
 
 //impressao
+void imprimirArray(int arr[], int n) {
+   
+    
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+}
 
 
 int main (){
     int arr[] = {12, 11, 13, 5, 6};
     int n = 5;
+
+    insertionSort(arr, n);
+    printf("valores ordenados: ");
+    imprimirArray(arr, n);
 
     return 0;
 }
